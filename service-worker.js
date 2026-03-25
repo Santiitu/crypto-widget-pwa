7ec9e0f291118dba1e0dc80ea04c0843bb81670c
Santiitu <77648929+Santiitu@users.noreply.github.com>
2026-03-25 13:51:25 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('crypto-pwa').then(c=>c.addAll(['/','/index.html','/app.js','/css/styles.css'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
