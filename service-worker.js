a55885c84da901ec296139cb6904107833d8cd56
Santiitu <77648929+Santiitu@users.noreply.github.com>
2026-03-25 14:48:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('crypto-final').then(c=>c.addAll(['/','/index.html','/app.js','/css/styles.css'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("crypto-final").then(cache =>
+      cache.addAll(["/", "/index.html", "/app.js", "/css/styles.css"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
